06f5802be827e7d07d30ef460f248410b1c04906
mremondi <mremondi@colby.edu>
2016-11-12 13:50:12 -0500

    got rid of unnecessary api calls

--- app/src/main/java/com/cravings/network/CraveAPI.java
+++ app/src/main/java/com/cravings/network/CraveAPI.java
@@ -5,10 +5,8 @@ import com.cravings.data.MenuItem;
 import com.cravings.data.Rating;
 import com.cravings.data.Restaurant;
 import com.cravings.data.User;
-
 import java.util.List;
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
@@ -16,13 +14,7 @@ import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
-
-/**
- * Created by mremondi on 8/1/16.
- */
 public interface CraveAPI {
-    @GET("restaurants")
-    Call<List<Restaurant>> getRestaurants();
 
     @GET("restaurants/{id}")
     Call<Restaurant> getRestaurantById(@Path("id") String id);
@@ -30,12 +22,8 @@ public interface CraveAPI {
     @GET("restaurants/{latitude}/{longitude}")
     Call<List<Restaurant>> getNearbyRestaurants(@Path("latitude") Double lat, @Path("longitude") Double lon);
 
-
     @GET("restaurants/search/{query}")
     Call<List<Restaurant>> searchRestaurants(@Path("query") String query);
-
-    @GET("menus")
-    Call<Menu> getMenus();
 
     @GET("menus/{id}")
     Call<Menu> getMenuById(@Path("id") String id);
@@ -45,9 +33,6 @@ public interface CraveAPI {
 
     @GET("menus_items/{id}/{section}")
     Call<List<MenuItem>> getMenuItemsBySection(@Path("id") String id, @Path("section") String section);
-
-    @GET("items")
-    Call<List<MenuItem>> getItems();
 
     @PUT("items/{id}/{rating}/{user_id}")
     Call<MenuItem> rateItem(@Path("id") String id, @Path("rating") float rating, @Path("user_id") String user_id);
@@ -61,9 +46,6 @@ public interface CraveAPI {
     @GET("items/search/{query}/{filter}/{latitude}/{longitude}")
     Call<List<MenuItem>> searchItemsSorted(@Path("query") String query, @Path("latitude") Double lat,
                                            @Path("longitude") Double lon, @Path("filter") String filter);
-
-    @GET("items/search/{menu_id}")
-    Call<List<MenuItem>> searchForMenuItems(@Path("menu_id") String menu_id);
 
     @FormUrlEncoded
     @POST("login")
